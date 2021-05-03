ab1fd4050f54e58c00028da5bc816b2dc8e7d193
Madhuri-Manvi <madhurimanvi@gmail.com>
2021-05-03 22:37:08 +0530

    Login page created

---
+++ src/screens/Controller.js
@@ -0,0 +1,27 @@
+import React,{Component} from 'react';
+import {BrowserRouter as Router,Route} from 'react-router-dom';
+
+import Login from './login/Login';
+
+
+
+//Creating controller class for easy routing the pages
+class Controller extends Component{
+    constructor(){
+        super()
+        this.baseUrl = "https://api.instagram.com/v1/users/self/"; //setting the baseUrl of the api 
+    }
+
+    render(){
+        return(
+            <Router>
+                <div className = 'main-container'>
+                    <Route exact path = '/' render={(props) => <Login {...props} baseUrl = {this.baseUrl}/>}/>           {/* Route to login Page */ }
+                
+                </div>
+            </Router>
+        )
+    }
+}
+
+export default Controller;
